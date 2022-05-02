26ebc466fd37038580771c2765fafc278fc2de7b
krogo81611 <krogo81611@gmail.com>
2022-05-02 14:29:17 -0600

    Commit Auth.js changes

--- auth/Auth.js
+++ auth/Auth.js
@@ -1,3 +1,21 @@
 const User = require('../model/User');
-const app = express();
-app.use(express.json());
+
+exports.register = async (req, res, next) => {
+    const {username, password} = req.body;
+    if (password.length < 6) {
+        return res.status(400).json({message: "password less than 6 characters"})
+    }
+    try {
+        await User.create({
+            username,
+            password,
+        }).then(user => 
+            res.status(200).json({
+                message: "User successfully created",
+                user,
+            })
+        )
+    } catch {
+        
+    }
+}
