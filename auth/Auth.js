3986ced19543038cb7673273ffb9c46549be87e8
krogo81611 <krogo81611@gmail.com>
2022-05-04 15:43:55 -0600

    Validate if username or password is empty

--- auth/Auth.js
+++ auth/Auth.js
@@ -22,3 +22,15 @@ exports.register = async (req, res, next) => {
         })
     }
 }
+
+exports.login = async(req, res, next) => {
+    const {username, password} = req.body;
+    if (!username || !password) {
+        return res.status(400).json({
+            message: "Username or password is empty",
+        })
+    }
+    try {
+        
+    }
+}
