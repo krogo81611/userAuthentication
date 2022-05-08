d0de10b05378070fca51a978b115c56a91e6c592
krogo81611 <krogo81611@gmail.com>
2022-05-07 18:47:10 -0600

    put not post

--- auth/Route.js
+++ auth/Route.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const {register, login} = require('./Auth');
+const {register, login, update} = require('./Auth');
 router.route('/register').post(register);
 router.route('/login').post(login);
-router.route('/update').post(update);
+router.route('/update').put(update);
 module.exports = router;
