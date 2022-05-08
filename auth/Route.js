59454dd35159793c28a1808050433b9cb60da14e
krogo81611 <krogo81611@gmail.com>
2022-05-07 18:46:00 -0600

    Added update route

--- auth/Route.js
+++ auth/Route.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const {register, login} = require('./Auth');
 router.route('/register').post(register);
 router.route('/login').post(login);
+router.route('/update').post(update);
 module.exports = router;
