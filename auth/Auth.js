1f45cab49311afe4959a8c1a6751bd3c483edf85
krogo81611 <krogo81611@gmail.com>
2022-05-08 19:09:36 -0600

    moved parenthese

--- auth/Auth.js
+++ auth/Auth.js
@@ -11,13 +11,13 @@ exports.register = async (req, res, next) => {
         await User.create({
             username,
             password: hash,
-        }).then(user => 
+        })).then(user => 
             res.status(200).json({
                 message: "User successfully created",
                 user,
             })
         )
-    )} catch (err) {
+    } catch (err) {
         res.status(401).json({
             message: "User creation failed",
             error: error.message,
