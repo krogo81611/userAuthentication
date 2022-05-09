268a5adf3a1670ea10ea7498c543f22f8d0132ba
krogo81611 <krogo81611@gmail.com>
2022-05-08 19:07:51 -0600

    added hash, salted hash

--- auth/Auth.js
+++ auth/Auth.js
@@ -7,16 +7,17 @@ exports.register = async (req, res, next) => {
         return res.status(400).json({message: "password less than 6 characters"})
     }
     try {
+        bcrypt.hash(password,10).then(async(hash) =>
         await User.create({
             username,
-            password,
+            password: hash,
         }).then(user => 
             res.status(200).json({
                 message: "User successfully created",
                 user,
             })
         )
-    } catch (err) {
+    )} catch (err) {
         res.status(401).json({
             message: "User creation failed",
             error: error.message,
