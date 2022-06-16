3b86d28c9ca4f7722edda8e4a04b0cf841590fa6
krogo81611 <krogo81611@gmail.com>
2022-06-15 18:13:27 -0600

    Changes

--- server.js
+++ server.js
@@ -11,6 +11,9 @@ app.use(express.json()); //Have application use express
 app.use(cookieParser());
 app.use('/api/Auth', require('./Auth/Route')); //Custom middleware import
 
+app.get('/admin', adminAuth, (req, res) => res.send("Admin Route"));
+app.get('/basic', userAuth, (req,res) => res.send("User Route"))
+
 const server = app.listen(PORT, () => console.log(`Server Connected to port ${PORT}`));
 
 process.on('unhandledRejection', err => {
