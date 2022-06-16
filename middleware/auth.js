2ebc7364013cc6a9ad1bbea47b4c6711142a6f6d
krogo81611 <krogo81611@gmail.com>
2022-06-15 18:07:35 -0600

    added user authentication

--- middleware/auth.js
+++ middleware/auth.js
@@ -20,3 +20,23 @@ exports.adminAuth = (req, res, next) => {
         .json({message: "Not authorized, token expired"})
     }
 }
+exports.userAuth = (req, res, next) => {
+    const token = req.cookie.jwt;
+    if (token) {
+        jwt.verify(token, jwtSecret, (err, decodedToken) => {
+            if(err) {
+                return res.status(401).json({message: "Not Authorized"})
+            } else {
+                if (decodedToken.role !== 'Basic') {
+                    return res.status(401).json({message: "Not authorized"})
+                } else {
+                    next();
+                }
+            }
+        });
+    } else {
+        return res
+        .status(401)
+        .json({message: "Not authorized, token expired"})
+    }
+}
