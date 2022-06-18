25fd000147d4360c01998c45c1d2e5b18059137e
krogo81611 <krogo81611@gmail.com>
2022-06-17 18:20:36 -0600

    Added app.get routes

--- server.js
+++ server.js
@@ -11,8 +11,11 @@ app.use(express.json()); //Have application use express
 app.use(cookieParser());
 app.use('/api/Auth', require('./Auth/Route')); //Custom middleware import
 
-app.get('/admin', adminAuth, (req, res) => res.send("Admin Route"));
-app.get('/basic', userAuth, (req,res) => res.send("User Route"))
+app.get('/', (req,res) => res.render('home'));
+app.get('/register', (req,res) => res.render('register'));
+app.get('/login', (req,res) => res.render('login'));
+app.get('/admin', adminAuth, (req, res) => res.render("admin"));
+app.get('/basic', userAuth, (req,res) => res.send("user"));
 
 const server = app.listen(PORT, () => console.log(`Server Connected to port ${PORT}`));
 
