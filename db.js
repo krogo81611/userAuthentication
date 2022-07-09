f8893a36598a36ce5ac3d8421494b60f4bcccc75
krogo81611 <krogo81611@gmail.com>
2022-07-09 13:52:32 -0600

    commit db.js file

---
+++ db.js
@@ -0,0 +1,10 @@
+const Mongoose = require('mongoose');
+const RemoteDB = `mongodb+srv://rogotron:testPassword@cluster0.snxpl4f.mongodb.net/?retryWrites=true&w=majority`;
+const connectDB = async () => {
+    await Mongoose.connect(RemoteDB);
+    .then(client => {
+        console.log("Successfully Connected to Database");
+    })
+}
+
+module.exports = connectDB;
