cfed80c9fac42aaf620662ddf3c50d64afbc50ef
krogo81611 <krogo81611@gmail.com>
2022-07-09 13:55:27 -0600

    Installed Express and Nodemon

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Mongoose = require('mongoose');
 const RemoteDB = `mongodb+srv://rogotron:testPassword@cluster0.snxpl4f.mongodb.net/?retryWrites=true&w=majority`;
 const connectDB = async () => {
-    await Mongoose.connect(RemoteDB);
+    await Mongoose.connect(RemoteDB)
     .then(client => {
         console.log("Successfully Connected to Database");
     })
